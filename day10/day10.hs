e51df258986d3818c109e6d2962955c34594db9a
Tim Fennis <tim.fennis@nos.nl>
2022-02-21 14:01:22 +0100

    Clean up day10 code

--- day10/day10.hs
+++ day10/day10.hs
@@ -3,17 +3,18 @@ import Data.List (sort, tails, subsequences, groupBy)
 
 day10 = do
     adapters <- (\x -> 0:(maximum x + 3):x) <$> (\x -> map read x :: [Int]) <$> lines <$> readFile "day10/input"
-    print $ sort adapters
-    print $ product $ map (conv . length) $ filter (\x -> all (==1) x) $ groupBy (==) $ map (\(a:b:[]) -> b - a ) $ windowed 2 $ sort adapters
-    print $ let (a,b) = solvePartOne adapters
-            in a * b
+    putStrLn $ "Part one: " ++ (show $ solvePartOne adapters)
+    putStrLn $ "Part two: " ++ (show $ solvePartTwo adapters)
 
-solvePartOne :: [Int] -> (Int, Int)
-solvePartOne adapters = (ones, threes)
+solvePartOne :: [Int] -> Int
+solvePartOne adapters = ones * threes
     where 
         ones = length $ filter (1==) diffs
         threes = length $ filter (3==) diffs
         diffs = map (\(a:b:[]) -> b - a ) $ windowed 2 $ sort adapters
+
+solvePartTwo :: [Int] -> Int
+solvePartTwo = product . map (conv . length) . filter (\x -> all (==1) x) . groupBy (==) . map (\(a:b:[]) -> b - a ) . windowed 2 . sort
 
 conv :: Int -> Int
 conv 4 = 7
